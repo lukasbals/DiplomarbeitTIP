419bff928b11cf489c62c4eca9c9cfe41702d7a4
Lukas Bals <lukibals@gmx.at>
2015-07-13 10:14:21 +0200

    blabla

--- greeter.js
+++ greeter.js
@@ -1,5 +1,5 @@
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstname + " " + person.lastname;
 }
-var user = "Jane User";
+var user = { firstname: "Luca", lastname: "Dreier" };
 document.body.innerHTML = greeter(user);
