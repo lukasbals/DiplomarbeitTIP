109fa7b576b4b5602e5e93d308907a65edde4752
Lukas Bals <lukibals@gmx.at>
2015-07-13 16:24:13 +0200

    dreier grichtat

--- myapp/routes/dreier.js
+++ myapp/routes/dreier.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('dreier!');
+/* GET dreier1111 listing. */
+router.get('/1', function(req, res, next) {
+  res.send('dreier1111!');
+});
+
+/* GET Dreier2222 listing. */
+router.get('/2', function(req, res, next) {
+  res.send('dreier2222!');
 });
 
 module.exports = router;
