ca07ed1bde80c838b57cf6625f57387369a15579
Luca Dreier <lucadreier22@gmail.com>
2015-07-13 16:11:37 +0200

    Ignore

--- myapp/routes/dreier.js
+++ myapp/routes/dreier.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET dreier listing. */
-router.get('/1', function(req, res, next) {
-  res.send('dreier1111!');
-});
-
-/* GET dreier listing. */
-router.get('/2', function(req, res, next) {
-  res.send('dreier2222!');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('dreier!');
 });
 
 module.exports = router;
