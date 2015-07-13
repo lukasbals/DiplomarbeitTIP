152464458035a1f2e9647d100e521730e302ccf8
Lukas Bals <lukibals@gmx.at>
2015-07-13 15:00:46 +0200

    dreier 1 + 2

--- myapp/routes/dreier.js
+++ myapp/routes/dreier.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('dreier!');
+/* GET dreier listing. */
+router.get('/1', function(req, res, next) {
+  res.send('dreier1111!');
+});
+
+/* GET dreier listing. */
+router.get('/2', function(req, res, next) {
+  res.send('dreier2222!');
 });
 
 module.exports = router;
