ac2f920798859419b0171876c11a99160ab6d2f7
Luca Dreier <lucadreier22@gmail.com>
2015-07-14 16:38:42 +0200

    Final Commit for today

--- Dreier Tests/typings/tsd.d.ts
+++ Dreier Tests/typings/tsd.d.ts
@@ -1,7 +1,1 @@
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="devextreme/dx.devextreme.d.ts" />
-/// <reference path="serve-favicon/serve-favicon.d.ts" />
-/// <reference path="morgan/morgan.d.ts" />
-/// <reference path="cookie-parser/cookie-parser.d.ts" />
-/// <reference path="body-parser/body-parser.d.ts" />
+
