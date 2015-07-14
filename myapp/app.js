4bb539ca934a37d74af7ca5d36d0c64940cd54f0
Lukas Bals <lukibals@gmx.at>
2015-07-14 08:54:01 +0200

    error message

--- myapp/app.js
+++ myapp/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var dreier = require('./routes/dreier');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/dreier', dreier);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
