a247ba3cc2c513769810eb7eeb5e6955a55350c6
Luca Dreier <lucadreier22@gmail.com>
2015-07-14 09:14:23 +0200

    Dreier Tests

---
+++ Dreier Tests/public/javascripts/greeter.js
@@ -0,0 +1,21 @@
+var Student = (function () {
+    function Student(firstname, middleinitial, lastname) {
+        this.firstname = firstname;
+        this.middleinitial = middleinitial;
+        this.lastname = lastname;
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
+    return Student;
+})();
+function greeter(person) {
+    return "Hello, " + person.firstname + " " + person.middleinitial + ". " + person.lastname;
+}
+var button = document.getElementById("submit");
+button.onclick = function () {
+    //alert("hallo");
+    var firstname = document.getElementById('firstname').value;
+    var middleinitial = document.getElementById('middleinitial').value;
+    var lastname = document.getElementById('lastname').value;
+    var user = new Student(firstname, middleinitial, lastname);
+    document.getElementById('content').innerHTML = greeter(user);
+};
