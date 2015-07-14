63ce4de270b9e5415001009b1a360c763ef82d82
Lukas Bals <lukibals@gmx.at>
2015-07-14 12:50:59 +0200

    layout

---
+++ myapp/public/javascripts/controllers/TestCtrl.ts
@@ -0,0 +1,20 @@
+module TIP {
+  export interface TestScope extends ng.IScope {
+    vorname: number;
+    nachname: string;
+
+    getProduct(id: number): void;
+  }
+
+  export class TestCtrl {
+    constructor(private test: TestService, public $scope:TestScope){
+      $scope.getProduct = ():void=>{
+        this.test.getProduct($scope.vorname);
+      };
+    }
+  }
+
+  angular
+    .module("tip")
+    .controller("TestCtrl", ["Test", "$scope", TestCtrl]);
+}
---
+++ myapp/public/javascripts/services/TestService.ts
@@ -0,0 +1,15 @@
+module TIP {
+  export class TestService {
+    constructor(private $http: ng.IHttpService){
+
+    }
+
+    getProduct(id: number): ng.IHttpPromise<any> {
+      return this.$http.get("www.url.com");
+    }
+  }
+
+  angular
+    .module("tip")
+    .service("Test", ["$http", TestService]);
+}
