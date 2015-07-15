9c7b41d3ec1d65aaa34974fba297932a4c651354
Luca Dreier <lucadreier22@gmail.com>
2015-07-15 08:45:38 +0200

    Controller and users updated with at best doubtful code

--- Dreier Tests/public/javascripts/Controllers/TestCtrl.js
+++ Dreier Tests/public/javascripts/Controllers/TestCtrl.js
@@ -1,16 +1,7 @@
-var DREIER;
-(function (DREIER) {
-    var TestCtrl = (function () {
-        function TestCtrl(test, $scope) {
-            var _this = this;
-            this.test = test;
-            this.$scope = $scope;
-            $scope.getProduct = function () {
-                _this.test.getProduct($scope.vorname);
-            };
-        }
-        return TestCtrl;
-    })();
-    DREIER.TestCtrl = TestCtrl;
-    angular.module("dreier").controller("TestCtrl", ["Test", "$scope", TestCtrl]);
-})(DREIER || (DREIER = {}));
+angular.module('index', [])
+    .controller('TestCtrl', ['$scope', function($scope) {
+      $scope.text = {
+        text: 'guest',
+        word: /^\s*\w*\s*$/
+      };
+    }]);
