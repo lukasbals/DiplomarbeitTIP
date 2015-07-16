210fa9bd577d5be7fe0e5998f1db3692f891b94e
Lukas Bals <lukibals@gmx.at>
2015-07-16 10:46:20 +0200

    added sql db

--- myapp/app.js
+++ myapp/app.js
@@ -58,5 +58,21 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-module.exports = app;
+// var sqlite3 = require("sqlite3").verbose();
+// var db = new sqlite3.Database("balsDB.db");
+//
+// db.serialize(function() {
+//
+//   // db.run("CREATE TABLE if not exists user_info (info TEXT)");
+//   // var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
+//   // for (var i = 0; i < 10; i++) {
+//   //     stmt.run("Ipsum " + i);
+//   // }
+//   // stmt.finalize();
+//
+//   db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
+//       console.log(row.id + ": " + row.info);
+//   });
+// });
+//
+// db.close();
