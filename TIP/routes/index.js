01dc346eb1adedcfac79b6a4726b70f4b9913656
Lukas Bals <lukibals@gmx.at>
2015-07-29 13:22:32 +0200

    index.ts / js

---
+++ TIP/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'M.I.E.' });
+});
+router.get("/geschaeftspartner", function (req, res, next) {
+    res.render("geschaeftspartner", { title: "Geschäftspartner" });
+});
+router.get("/person", function (req, res, next) {
+    res.render("person", { title: "Personen" });
+});
+module.exports = router;
