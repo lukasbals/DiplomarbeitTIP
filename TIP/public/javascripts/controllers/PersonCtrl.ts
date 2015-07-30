1c678bb5cd3c4c71e4687160517e65028a883bad
Lukas Bals <lukibals@gmx.at>
2015-07-30 08:51:05 +0200

    Revert 8e034d4..d9bd4d6
    This rolls back to commit 8e034d475fa9b00ee7fe3a2d7d7d83f89d5a62b6.

---
+++ TIP/public/javascripts/controllers/PersonCtrl.ts
@@ -0,0 +1,107 @@
+module TIP {
+  export class PersonViewModel {
+    constructor(private my: MyService) {
+
+    }
+    //
+    // Detail Page
+    //
+
+    // detail-page person
+    detailPersonDataSource: JSON = null;
+    initDetailPerson() {
+      var id: number = this.getParameter("id");
+      /*alert(id + table)*/
+      this.my.getPersonDetail(id)
+        .success((data): void => {
+        this.detailPersonDataSource = data;
+      })
+        .error((data): void => {
+        console.log("Keine DetailDaten bekommen.")
+      });
+    }
+
+    getParameter = (theParameter): any => {
+      var params = window.location.search.substr(1).split('&');
+      for (var i = 0; i < params.length; i++) {
+        var p = params[i].split('=');
+        if (p[0] == theParameter) {
+          return decodeURIComponent(p[1]);
+        }
+      }
+      return false;
+    }
+
+    //
+    // Personen Page
+    //
+    dataSourcePerson: JSON = null;
+    initPerson() {
+      this.my.getPerson()
+        .success((data): void => {
+        this.dataSourcePerson = data;
+      })
+        .error((data): void => {
+        console.log("Keine Personen bekommen.");
+      });
+    }
+
+    gridPerson: any = {
+      //loadPanel: false,
+      columns: [{
+        dataField: 'CodeAnrede',
+        caption: "Anrede",
+        width: 70,
+        allowFiltering: false
+      }, {
+          dataField: 'Titel',
+          allowFiltering: false,
+          width: 90
+        }, {
+          dataField: 'Vorname'
+        }, {
+          dataField: 'Nachname'
+        }, {
+          dataField: 'Abteilung'
+        }, {
+          dataField: "Telefon",
+          width: 150,
+          allowFiltering: false
+        }, {
+          dataField: "Email",
+          width: 230,
+          allowFiltering: false
+        }],
+      columnAutoWidth: true,
+      bindingOptions: {
+        dataSource: "vm.dataSourcePerson"
+      },
+      paging: {
+        pageSize: 25
+      },
+      searchPanel: {
+        visible: true,
+        width: 250,
+        highlightSearchText: false,
+        placeholder: "Suchen..."
+      },
+      rowClick: (options): void => {
+        window.location.href = "http://localhost:3000/detail/detailPerson?id=" + options.data.Id;
+      }
+    }
+  }
+
+  export interface PersonScope extends ng.IScope {
+    vm: PersonViewModel;
+  }
+
+  export class PersonCtrl {
+    constructor(private my: MyService, public $scope: PersonScope) {
+      $scope.vm = new PersonViewModel(my);
+    }
+  }
+
+  angular
+    .module("tip")
+    .controller("PersonCtrl", ["MyService", "$scope", PersonCtrl]);
+}
