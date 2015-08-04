74ac38a2867bd111a0f6fb469023b26fe7a8a69f
Lukas Bals <lukibals@gmx.at>
2015-08-04 16:15:45 +0200

    unnötige raus

--- Devextreme/public/javascripts/Initialize.js
+++
@@ -1,7 +0,0 @@
-var TIP;
-(function (TIP) {
-    var clientLanguageToUse = navigator.language || navigator.browserLanguage;
-    Globalize.culture(clientLanguageToUse);
-    DevExpress.devices.current({ platform: "generic" });
-    angular.module("tip", ["ngSanitize", "dx"]);
-})(TIP || (TIP = {}));
