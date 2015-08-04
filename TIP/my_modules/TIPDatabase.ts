c026cced712a0a0643ca85a99b1dd7366f05caea
Lukas Bals <lukibals@gmx.at>
2015-08-04 16:15:00 +0200

    TIPDATABASEDABEI

---
+++ TIP/my_modules/TIPDatabase.ts
@@ -0,0 +1,31 @@
+import sqlite3 = require("sqlite3");
+//set connection to node.js Database
+var db = new sqlite3.Database("db.sql")
+
+module TIP {
+  export class TIPDatabaseImpl {
+    getDB(): sqlite3.Database {
+      return db;
+    }
+
+    setSYNCH(tblName: string, date: Date): void {
+      db.serialize((): void => {
+        db.run("create table if not exists synch_st (tabelle string(50), ltzt_synch_start TIMESTAMP)");
+
+        var d = date.toLocaleString();
+
+        //console.log(tblName);
+        db.get("select count(*) as result from synch_st where tabelle = ?;", [tblName], (error, row): void => {
+          //console.log(row.result);
+          if (row.result > 0) {
+            db.run("update synch_st set ltzt_synch_start = ? where tabelle = ?", [d, tblName])
+          } else {
+            db.run("insert into synch_st (tabelle, ltzt_synch_start) values (?, ?)", [tblName, d]);
+          }
+        });
+      });
+    }
+  }
+}
+
+module.exports = new TIP.TIPDatabaseImpl();
