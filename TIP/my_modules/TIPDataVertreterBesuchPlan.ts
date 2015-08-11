ba2d718b6d02c2fe09ebc450496ec17329372b3b
Lukas Bals <lukibals@gmx.at>
2015-08-11 15:18:04 +0200

    verschieben funktioniert unglaublich! aber nicht so ganz logisch

--- TIP/my_modules/TIPDataVertreterBesuchPlan.ts
+++ TIP/my_modules/TIPDataVertreterBesuchPlan.ts
@@ -100,14 +100,16 @@ module TIP {
     }
 
     updateBesuchPlanAppointment(id: number, startDate: Date, endDate: Date, id_geschaeftspartner: number, res): void {
-      var sD = startDate.toLocaleString();
-      var eD = endDate.toLocaleString();
-      console.log("Hier wird gloggt!");
-      console.log(id);
-      console.log(sD);
-      console.log(eD);
-      console.log(id_geschaeftspartner);
-      TIPDatabase.getDB().run("update besuche_plan set is_changed = 1, von = ?, bis = ?, id_geschaeftspartner = ? where id = ?", [sD, eD, id_geschaeftspartner, id]);
+      var x = new Date(startDate.toLocaleString());
+      var y = new Date(endDate.toLocaleString());
+      var sD = x.toISOString();
+      var eD = y.toISOString();
+      // console.log("Hier wird gloggt!");
+      // console.log(id);
+      // console.log(sD);
+      // console.log(eD);
+      // console.log(id_geschaeftspartner);
+      TIPDatabase.getDB().run("update besuche_plan set is_changed = 1, von = ?, bis = ?, id_geschaeftspartner = ? where client_id = ?;", [sD, eD, id_geschaeftspartner, id]);
       res.send("OK");
     }
   }
